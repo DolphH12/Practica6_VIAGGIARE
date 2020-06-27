c2a7d555b734b2c600a9d8f19f566dc041c86c64
Dolph B. Hincapie <dolph.hincapie@udea.edu.co>
2020-06-27 11:22:17 -0500

    Laboratorio 2 - Avance Viaggiare - Hecho por Dolph y Thomas

--- app/src/main/java/com/dolphhincapie/introviaggiare/fragments/UserFragment.kt
+++ app/src/main/java/com/dolphhincapie/introviaggiare/fragments/UserFragment.kt
@@ -4,29 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.dolphhincapie.introviaggiare.R
-import com.dolphhincapie.introviaggiare.ui.dashboard.DashboardViewModel
 
 class UserFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    //private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        //dashboardViewModel =
+            //ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_user, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        /*val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        })
+        })*/
         return root
     }
 }
